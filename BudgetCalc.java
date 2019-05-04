f5b85cfc0c509f90716d19cf42851d7bd9cf7e9d
JuleeyG <36057651+JuleeyG@users.noreply.github.com>
2019-05-04 15:15:36 -0400

    first commit

---
+++ BudgetCalc.java
@@ -0,0 +1,47 @@
+// Juleeyah Wright
+// Summer 2019
+// I say I never have the time to budget. It may be that I`m lazy, but ironically I coded up this budget
+// calculator to track my spendings. Hold your applause as I bow. Thank you  _   _  _____
+//                                                                        / ||  || 
+//                                                                        --| -- |
+//                                                                          ------
+//                                                                         /__    \
+
+import java.io.*;
+import java.util.*;
+
+public class BudgetCalc
+{
+    private HashSet<String> budget;
+    private int thisMonthsTotal = 0;
+    private String[] danero;
+
+    public String getBudget(String filename) throws IOException
+    {
+       Scanner input = new Scanner(new File(filename)); 
+       budget = new HashSet<String>();
+       int total = input.nextInt();
+
+        while (input.hasNext())
+        {
+            String next = input.nextLine();
+            if (next.contains("$"))
+            {
+                danero = next.split(" ");
+                budget.add(danero[0]);
+                thisMonthsTotal += Integer.parseInt(danero[0].substring(1));
+            }
+        }
+
+        input.close();
+        return "You will spend $" + thisMonthsTotal + " this month + have " + (total - thisMonthsTotal) + " left :)";
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        BudgetCalc budget = new BudgetCalc();
+
+        System.out.println(budget.getBudget("MonthlyBudget.txt"));
+    }
+}
+
